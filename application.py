f50760650ee8702cff69e745c0f99036b8ed4728
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2019-02-04 21:58:37 -0200

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from database_setup import Category, User, Item, Base
+from models import Category, User, Item, Base
 from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
@@ -19,14 +19,37 @@ app = Flask(__name__)
 # ADD a /users route here
 
 
-@app.route('/categories', methods=['GET', 'POST'])
-# protect this route with a required login
-def showAllCategories():
+@app.route('/categoryJSON', methods = ['GET','POST'])
+#protect this route with a required login
+def showAllCategoriesJSON():
     if request.method == 'GET':
         categories = session.query(Category).all()
-        return jsonify(categories=[
-                                  category.serialize
-                                  for category in categories])
+        return jsonify(categories = [category.serialize for category in categories])
+
+@app.route('/category', methods = ['GET','POST'])
+#protect this route with a required login
+def showAllCategories():
+	return "This Page will show all my categories"
+		
+@app.route('/category/<string:category_name>', methods = ['GET','POST'])
+#protect this route with a required login
+def showCategoryItems(category_name):
+	return "This Page is the items for category %s" % category_name
+		
+@app.route('/category/<string:category_name>/newitem', methods = ['GET','POST'])
+#protect this route with a required login
+def NewCategoryItem(category_name):
+	return "This page is for making a new Item for category %s" % category_name
+		
+@app.route('/category/<string:category_name>/<string:item_name>/edit', methods = ['GET','POST'])
+#protect this route with a required login
+def EditCategoryItem(category_name, item_name):
+	return "This Page will be editing Category Item  %S" % item_name
+		
+@app.route('/category/<string:category_name>/<string:item_name>/delete', methods = ['GET','POST'])
+#protect this route with a required login
+def DeleteCategoryItem(category_name, item_name):
+	return "This Page will be deleting a Category Item  %s" % item_name
 
 if __name__ == '__main__':
     app.debug = True
