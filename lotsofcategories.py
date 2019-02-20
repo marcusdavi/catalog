219a3aebf4824bd7557e6dd999e7ff4ad5b8bd6f
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2019-02-19 21:59:15 -0300

    Update lotsofcategories.py

--- lotsofcategories.py
+++ lotsofcategories.py
@@ -68,7 +68,7 @@ session.commit()
 
 # ADD User
 user1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
-             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')  # noqa
 
 session.add(user1)
 session.commit
